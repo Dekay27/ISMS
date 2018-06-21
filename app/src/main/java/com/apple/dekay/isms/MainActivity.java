d33a94147a14d72a2391ef676609b29cbe6323a2
Dekay27 <dannykumah27@gmail.com>
2018-06-21 16:05:27 +0000

    June 21_Legon

--- app/src/main/java/com/apple/dekay/isms/MainActivity.java
+++ app/src/main/java/com/apple/dekay/isms/MainActivity.java
@@ -6,21 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     private static int SPLASH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent homeIntent = new Intent(MainActivity.this, selectScreen.class);
                 startActivity(homeIntent);
-                finish()
+                finish();
             }
         },SPLASH);
+
     }
 }
--- app/src/main/java/com/apple/dekay/isms/selectScreen.java
+++ app/src/main/java/com/apple/dekay/isms/selectScreen.java
@@ -1,13 +1,58 @@
 package com.apple.dekay.isms;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class selectScreen extends AppCompatActivity {
 
+    Button parent;
+    Button student;
+    Button staff;
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_screen);
+
+        parent = findViewById(R.id.parent);
+        student = findViewById(R.id.student);
+        staff = findViewById(R.id.staff);
+
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Page Not Available",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        staff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Page Not Available",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    /** Called when the user taps the Send button */
+    public void parentClick(View view) {
+        // Do something in response to button
+        Toast.makeText(getApplicationContext(),
+                "Welcome",Toast.LENGTH_SHORT).show();
+        Intent loginIntent = new Intent(selectScreen.this, login.class);
+        startActivity(loginIntent);
+        }
+
+
 }
+
+
+
